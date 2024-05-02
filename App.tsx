f6284b26ef01299f44ffc8da7ff63e35f61214a8
lucirs7 <luciars787@gmail.com>
2024-05-02 11:34:46 +0200

    Video #27. Handling iphone notch and android notification bar.
    Used component 'safeAreaView'.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{marginTop: 25}}>
+        <Text style={{fontSize: 25, color: 'white'}}>Test!</Text>
+        <StatusBar style='auto' />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'rgb(100, 100, 275)',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
   },
 });
